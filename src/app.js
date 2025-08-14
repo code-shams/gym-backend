079db9395b2cfe50fba97da1b3b34db8a75d948d
code-shams <code.meshams@gmail.com>
2025-08-14 12:52:01 +0600

    add: role based middleware added

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const authRoutes = require("./routes/auth.routes");
 dotenv.config();
 connectDB();
 
+//? Import routes
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+
 const app = express();
 app.use(express.json());
+
+//? Routes
 app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
 
 //? Health check route
 app.get("/", (req, res) => res.send("Gym API Running"));
---
+++ src/middlewares/role.middleware.js
@@ -0,0 +1,17 @@
+const authorizeRoles = (...allowedRoles) => {
+    return (req, res, next) => {
+        if (!req.user) {
+            return res.status(401).json({ message: "User not authenticated" });
+        }
+
+        if (!allowedRoles.includes(req.user.role)) {
+            return res
+                .status(403)
+                .json({ message: "Forbidden: Access denied" });
+        }
+
+        next();
+    };
+};
+
+module.exports = authorizeRoles;
